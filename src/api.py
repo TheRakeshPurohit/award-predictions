7810d5bc44bcf9dfe6dde0c2ef832c627c848453
srhm <me@srhm.ca>
2023-04-06 17:48:24 -0400

    thing

--- src/api.py
+++ src/api.py
@@ -1,46 +1,65 @@
-"""Description of your app."""
+"""A story tagger designed to return metadata for a given input summary. By Sil."""
 from enum import Enum
 from typing import Optional, List, Dict, Any
-from steamship import Steamship
+from steamship import Steamship, File, Block
 from steamship.invocable import post, PackageService, InvocationContext
 
 from pydantic import BaseModel
-
-class Style(str, Enum):
-     painting = "painting"
-     neon = "neon"
-     magazine = "magazine"
+from retry import retry
 
 
 class Output(BaseModel):
-    image_urls: Optional[List[str]]
-    text: Optional[str]
+    setting: Optional[str]
+    character: Optional[str]
+    genre: Optional[str]
+    logline: Optional[str]
+    tag: Optional[str]
+    ifyoulike: Optional[str]
 
 
 class SteamshipPackage(PackageService):
     def __init__(
         self,
-        client: Steamship = None,
-        config: Dict[str, Any] = None,
+        client: Steamship = "sil",
+        config: Dict[str, any] = None,
         context: InvocationContext = None,
     ):
         super().__init__(client, config, context)
 
-    @post("generate")
-    def generate(
-        self,
-        title: str = "A Story", 
-        description: str = "Story Description",
-        style: Style = Style.painting
-    ) -> dict:
 
-        # CODE THAT DOES THINGS HERE :)
+    @retry(SyntaxError, tries=3)
+    def classify(self, job, tagger) -> dict:
+        tag_task = job.tag(tagger.handle)
+        tag_task.wait()
+        job.refresh()
+        return eval(job.blocks[0].tags[0].value.get(TagValueKey.STRING_VALUE.value))
+
+
+    @post("generate")
+    def generate(self, summary: str = "Story Summary") -> dict:
+
+        job = File.create(self.client, blocks = [Block(text = summary)])
+
+        tagger = self.client.use_plugin(
+            plugin_handle = "prompt-generation-trainable-default",
+            version = '0.0.22',
+            config = {"max_words" : 600,
+                      "openai_api_key" : "",
+                      "tag_kind": "training_generation",
+                      "temperature": 0.5,
+                      "num_completions": 1},
+            instance_handle = "curie-label")
+
+        tags = self.classify(job, tagger)
+
+        job.delete()
 
         output = Output(
-            text="This is text!",
-            image_urls=[
-                "https://media.istockphoto.com/id/1157515115/photo/cheeseburger-isolated-on-white.jpg?s=612x612&w=0&k=20&c=6f6jnWe3iGi2GinEvSJlDsqKbaYoRwj3vYChPCU96U4="
-            ]   
-        )
+            setting = tags["setting"],
+            character = tags["character"],
+            genre = tags["genre"],
+            logline = tags["logline"],
+            tag = tags["tag"],
+            ifyoulike = tags["ifyoulike"])
 
         return output
